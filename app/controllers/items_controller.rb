2779c9add976ff2d4f07696696dfd85b55a9eccf
Olivia Pomeroy <opomeroy26@gmail.com>
2022-04-06 10:21:28 -0700

    commit tree d6a45f87e83d6a10bf5459d16ed9912dd7df8b7a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show 
+    item = Item.find(params[:id])
+    render json: item 
+  end
+
+  def create 
+    user = find_user 
+    item = user.items.create(item_params)
+    render json: item, status: :created 
+  end
+
+  private 
+
+  def item_params 
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response(invalid)
+    render json: {error: `#{invalid.model} not found`}, status: :not_found
   end
 
 end
